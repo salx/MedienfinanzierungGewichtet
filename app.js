3761fc1bcf05f9d886de5e20ff6f6ef90963e73c
Alexandra Siebenhofer <salx@0xx0.net>
2014-01-28 08:45:21 +0100

    final version 1

--- app.js
+++ app.js
@@ -11,28 +11,29 @@ Vergleiche DE außerhalb der Grafik?
 */
 
 (function(){
-	var margin = {top: 20, right: 20, bottom: 200, left: 70},
+	var margin = {top: 100, right: 20, bottom: 20, left: 70},
         width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 500 - margin.top - margin.bottom,
+        half = height/2-1;
 
     //ordinal scale for medienunternehmen
     var x0 = d3.scale.ordinal()
-    	.rangeRoundBands([0, width, 0.2]);
+    	.rangeRoundBands([0, width], 0.2);
 
     //second x scale for grouping
     var x1 = d3.scale.ordinal();
 
     //vertikal scale for umsatz und gewinn
     var y0 = d3.scale.linear()
-    	.range([height, 0]);
+    	.range([half, 0]);
 
     //second vertikal scale for mitarbeiter
     var y1 = d3.scale.linear()
-    	.range([height, 0]);
+    	.range([0, half]);
 
     // color Scale
     var color = d3.scale.ordinal()
-    	.range(["#98abc5", "#ff8c00"])
+    	.range(["#98abc5", "#6b486b"])
 
     //Axes
     var xAxis = d3.svg.axis()
@@ -46,7 +47,7 @@ Vergleiche DE außerhalb der Grafik?
 
     var yAxis2 = d3.svg.axis()
     	.scale(y1)
-    	.orient("right")
+    	.orient("left")
 
     var svg = d3.select("body")
     	.append("svg")
@@ -55,24 +56,35 @@ Vergleiche DE außerhalb der Grafik?
     	.append("g")//welchen Sinn macht das, wenn nachher auch noch eine gruppe reinkommt?
     	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.csv("Medienfinanzierung2.csv", function(error, data){
+    d3.csv("Medienfinanzierung3.csv", function(error, data){
+
     	var mediaNames = d3.keys(data[0]).filter( function(key){ return key !== "Unternehmen"; } ); 
 
     	data.forEach(function(d){
     		d.mediaValues = mediaNames.map(function(name){ return {name: name, value: +d[name]}; }); 
     	});
 
-    	data.sort(function(a, b){ return b-a; });//wie die Werte angeben...
+    	//data.sort(function(a, b){ return b-a; });//wie die Werte angeben...
 
     	//set the domains
     	x0.domain(data.map(function(d) { return d.Unternehmen } ));
     	x1.domain(mediaNames).rangeRoundBands([0, x0.rangeBand()]); //check
-    	y0.domain([0, d3.max(data, function(d) { return d3.max(d.mediaValues, function(d){ return d.value }); })]);
-    	//y1.domain()
+    	
+    	function key( name ) {
+    		return function(d) {
+    			return d[name];
+    		}
+    	}
 
+    	var umsaetze = data.map( key('mediaValues') ).map( function( d ) { return d[0] } ).map( key( 'value') );
+    	var mitarbeiter = data.map( key('mediaValues') ).map( function( d ) { return d[1] } ).map( key( 'value') );
+    	y0.domain([0, d3.max(umsaetze)]);
+    	y1.domain([0, d3.max(mitarbeiter)])
+
+    	/*
     	svg.append("g")
     		.attr("class", "x axis")
-    		.attr("transform", "translate(-12," + (height + 7)+ ")")
+    		.attr("transform", "translate(0," + (height+10) + ")")
     		//.attr("transform", "translate(0,360)")
     		.call(xAxis)
     		.selectAll("text")
@@ -80,6 +92,15 @@ Vergleiche DE außerhalb der Grafik?
     		.attr("transform", "rotate(-90)")
     		.attr("dx", "-0.71em")
 			.attr("dy", "-0.31em");
+		*/
+		
+
+		svg.append( 'line' )
+			.attr("class", "line")
+			.attr("x0", 0 )
+			.attr("x1", width )
+			.attr("y1", half )
+			.attr("y2", half );
 
     	svg.append("g")
     		.attr("class", "y axis")
@@ -91,23 +112,53 @@ Vergleiche DE außerhalb der Grafik?
     		.style("text-anchor", "end")
     		.text("Umsatz in Mio. €");
 
+    	svg.append("g")
+    		.attr("class", "y axis")
+    		.attr('transform', 'translate(0,' + half + ')')
+    		.call(yAxis2)
+       		.append("text")
+    		.attr("transform", "rotate(-90)")
+    		.attr("y", 6)
+    		.attr("dy", "0.71em")
+    		.style("text-anchor", "end")
+    		.text("Mitarbeiter");
+
     	var unternehmen = svg.selectAll(".unternehmen")
     		.data(data)
-    		.enter().
-    		append("g")
-    		.attr("class", "g")
+    		.enter()
+    		.append("g")
+    		.attr("class", "unternehmen")
     		.attr("transform", function(d){ return "translate(" + x0(d.Unternehmen) + ", 0)" });
 
     	unternehmen.selectAll("rect")
     		.data(function(d){ return d.mediaValues; })
     		.enter()
     		.append("rect")
-    		.attr("width", x1.rangeBand())
-    		.attr("x", function(d){ return x1(d.name); })
-    		.attr("y", function(d){ return y0(d.value); })
-    		.attr("height", function(d){ return height - y0(d.value) })
+    		.attr("width", x0.rangeBand())
+    		//.attr("x", function(d){ return x0(d.name); })
+    		.attr("y", function(d){ 
+    		    if( d.name === 'Mitarbeiter' ) {
+                    return half+1;
+    		    } else {
+    		    	return y0(d.value);
+    		    }
+    			
+    		})
+    		.attr("height", function(d){
+    		 if( d.name === "Mitarbeiter" ){
+    		 	return half - ( half - y1(d.value) )
+    		 }
+    			return half - y0(d.value)
+    		})
     		.attr("fill", function(d){ return color(d.name); });
 
+    	unternehmen.append( 'text' )
+    	    .attr( 'class', 'label' )
+    	    .attr( 'y', x0.rangeBand() / 2 )
+    	    .attr( 'x', function(d) { return 0-y0(d.mediaValues[0].value) +5 })
+    	    .attr("text-anchor", "start")
+    	    .attr("transform", "rotate(-90)")
+    	    .text( function(d) { return d.Unternehmen } );
 
     })
 
